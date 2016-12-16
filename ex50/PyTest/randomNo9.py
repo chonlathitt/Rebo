193920a3d8a261b6e3a56e9328c5fac60561b19c
chonlathit <ke.chonlathit_st@tni.ac.th>
2016-12-16 04:22:22 +0000

    aodd

---
+++ ex50/PyTest/randomNo9.py
@@ -0,0 +1,5 @@
+import random
+
+num = random.randint(0,99999)
+
+print(num*10+9)
--- rebo.py
+++ rebo.py
@@ -1,8 +1,18 @@
-class printLotto:
-	# lotto_1st เป็นรางวัลที่ 1 
+import random
 
-	def run_1st():
-	
-		# 
+num = random.randint(0,999999)
+num1 = random.randint(0,10)
+num2 = random.randint(0,10)
+num3 = random.randint(0,10)
 
-		return lotto_1st
+print(num)
+
+lotto = input("lotto3 : ")
+print (lotto*10+num1)
+
+lotto1 = input("lotto4 : ")
+print (lotto1*10+num2)
+
+lotto2 = input("lotto1 : ")
+print (lotto2*10+num3)
+
